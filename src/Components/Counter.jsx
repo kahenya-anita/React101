dce8eb5bc996560d88991e7540945fbfa3a0991a
anita-that-chiq <62019551+kahenya-anita@users.noreply.github.com>
2025-04-23 09:58:13 +0300

    Changes

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -11,7 +11,7 @@ function Counter() {
   return (
     <div>
         <h1>Learning State using Click Counter</h1>
-        <p>You have clicked the button: {count} times</p>
+        <p>You have clicked the button: {count} times ♻️</p>
         <button onClick={handleClick}>Click Me</button>
     </div>
   )
